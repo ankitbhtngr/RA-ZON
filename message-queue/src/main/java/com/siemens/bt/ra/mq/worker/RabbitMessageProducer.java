5257939fdd18b7d6171cb45db748782af657f7a9
saurabhdevops <saurabh.devops@gmail.com>
2016-12-20 08:25:46 +0000

    Unzipping and restructuring the project

--- message-queue/src/main/java/com/siemens/bt/ra/mq/worker/RabbitMessageProducer.java
+++ message-queue/src/main/java/com/siemens/bt/ra/mq/worker/RabbitMessageProducer.java
@@ -13,30 +13,30 @@ import com.siemens.bt.ra.mq.common.ChannelCreator;
 
 public class RabbitMessageProducer implements MessageProducer {
 
-  private static String TASK_QUEUE_NAME = "default_queue";
+	private static String TASK_QUEUE_NAME = "default_queue";
 
-  @Override
-  public void produce(String topic, String message) throws IOException {
-    TASK_QUEUE_NAME = topic;
-    Channel channel = createChannel(topic);
-    publish(message, channel);
-    closeChannel(channel);
-  }
+	@Override
+	public void produce(String topic, String message) throws IOException {
+		TASK_QUEUE_NAME = topic;
+		Channel channel = createChannel(topic);
+		publish(message, channel);
+		closeChannel(channel);
+	}
 
-  private static void closeChannel(Channel channel) throws IOException {
-    channel.close();
-    channel.getConnection().close();
-  }
+	private static void closeChannel(Channel channel) throws IOException {
+		channel.close();
+		channel.getConnection().close();
+	}
 
-  private static void publish(String message, Channel channel) throws IOException {
-    channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
-    System.out.println(" [x] Sent '" + message + "'");
-  }
+	private static void publish(String message, Channel channel) throws IOException {
+		channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+		System.out.println(" [x] Sent '" + message + "'");
+	}
 
-  private static Channel createChannel(String topic) throws IOException {
-    TASK_QUEUE_NAME = topic;
-    Channel channel = ChannelCreator.create();
-    channel.queueDeclare(TASK_QUEUE_NAME, true);
-    return channel;
-  }
+	private static Channel createChannel(String topic) throws IOException {
+		TASK_QUEUE_NAME = topic;
+		Channel channel = ChannelCreator.create();
+		channel.queueDeclare(TASK_QUEUE_NAME, true);
+		return channel;
+	}
 }
